1c35bf7f365ecea144fcb618f1563c959e3e86d5
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-05-13 18:14:13 +0200

    fine prima parte

--- script.js
+++ script.js
@@ -1,4 +1,70 @@
-// l'utente sceglie il livello di difficolta 
-// l'utente manda l'input 
-// si crea la griglia a seconda della difficolta
-// si crea la funizone che interagisce con i quadrati 
+const playGame = document.getElementById("play");
+// funzione che da inizio al gioco
+playGame.addEventListener("click", inizioGame);
+
+
+//gestore che inizializza il gioco
+function inizioGame() {
+    //Variabili griglia e difficolta selettore
+    const grid = document.getElementById("grid");
+    const difficultyOption = document.getElementById("sezione_difficolta");  
+    const difficulty = difficultyOption.value;
+
+    // funzione difficolta
+    userDifficuly(grid, difficulty);
+
+
+}
+
+
+
+
+//Gestore delle difficoltà
+function userDifficuly(grid, difficulty) {
+    if (difficulty == 1) {
+        gridGenerator(grid, 100, "facile"); //facile
+    } else if (difficulty == 2) {
+        gridGenerator(grid, 81, "medio"); //Medio
+    } else {
+        gridGenerator(grid, 49, "difficile"); //difficile
+    }
+}
+
+
+function gridGenerator(grid, gridTot, className) {
+
+    //Svuota la griglia
+    grid.innerHTML = "";
+
+    for (let i = 1; i <= gridTot; i++) {
+        //Genera Square
+        let gridSquare = gridSquareGen(className, i);
+
+        //Aggiunge eventListener al click
+        gridSquare.addEventListener("click", activeSquare);
+
+        //Aggiunge gridSquare alla griglia
+        grid.append(gridSquare);
+    }
+}
+
+//Generatore di square e numeri
+function gridSquareGen(className, i) {
+    //Crea square
+    let gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid_square");
+    gridSquare.classList.add(className);
+
+    //Inserisce numero 
+    let gridSquareNum = document.createElement("div");
+    gridSquareNum.classList.add("grid_number");
+    gridSquareNum.innerHTML = i;
+    gridSquare.append(gridSquareNum);
+
+    return gridSquare;
+}
+
+//Aggiunge la classe "active" ad un elemento
+    function activeSquare() {
+     this.classList.add("active");
+ } 
